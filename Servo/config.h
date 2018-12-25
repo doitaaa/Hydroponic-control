0022447440fd6f6e15f8f439dd677c004308f5b7
doitaa <derdetdey@gmail.com>
2018-12-25 17:09:29 +0200

    configuration file for servos and pumps
    seperate files to store configurations before runtime

---
+++ Servo/config.h
@@ -0,0 +1,26 @@
+/*
+ * config.h
+ * configuration file for servos and pumps
+ * Created: 25-Dec-18 4:29:06 PM
+ *  Author: mohamed
+ */ 
+
+
+#ifndef CONFIG_H_
+#define CONFIG_H_
+
+#define servoPh_pin D,4
+#define servoNutrients_pin D,5
+#define _Ph 1
+#define _Nutrients 2
+
+#define pumpPhPlus_StepPin
+#define pumpPhPlus_DirPin
+#define pumpPhMinus_StepPin
+#define pumpPhMinus_DirPin
+#define pumpNutrients_StepPin
+#define pumpNutrients_DirPin
+
+
+
+#endif /* CONFIG_H_ */
