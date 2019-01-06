9ea7ad1a2c04a17e5b74ea786f10701f0a108684
doitaa <derdetdey@gmail.com>
2019-01-06 20:58:47 +0200

    config file alterations

--- Servo/config.h
+++ Servo/config.h
@@ -8,12 +8,11 @@
 
 #ifndef CONFIG_H_
 #define CONFIG_H_
-
+//servo pins configurations
 #define servoPh_pin D,4
-#define servoNutrients_pin D,5
-#define _Ph 1
-#define _Nutrients 2
+#define servoEC_pin D,5
 
+//pumps pins configurations for stepper motor drivers
 #define pumpPhPlus_StepPin
 #define pumpPhPlus_DirPin
 #define pumpPhMinus_StepPin
